170e0664baed001897c13f99638c36e056cc7f2d
YigitKACARAN <102469468+YigitKACARAN@users.noreply.github.com>
2023-05-31 23:10:42 +0300

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -27,7 +27,7 @@ export default function Index() {
               </a>
             </Link>
   <div className="py-10"></div>
-  <Link href={"https://discord.com/api/oauth2/authorize?client_id=1097388579312910417&permissions=8&scope=bot%20applications.commands"}>
+  <Link href={"topgggggg"}>
   <a className={" px-6 justify-center gap-x-2 shadow-lg shadow-amber-600/20 rounded-xl py-4 font-medium bg-gradient-to-bl from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white " }>
 Vote Spotify Music</a>
   </Link>
