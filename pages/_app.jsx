5361ea54d63654b868f044aad92a73b0db8a77de
YigitKACARAN <102469468+YigitKACARAN@users.noreply.github.com>
2023-05-31 22:06:47 +0300

    Update _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -52,13 +52,6 @@ export default function AwardApp({ Component, pageProps }) {
       icon: "fa fa-plus",
       activeIcon: "fa fa-plus",
       href: "https://top.gg/bot/774043716797071371/vote",
-    },
-    {
-      link: true,
-      name: "Partners",
-      icon: "fal fa-handshake",
-      activeIcon: "fa fa-handshake",
-      href: "/partners",
     }
   ]
 
